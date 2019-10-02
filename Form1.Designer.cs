aa6041834b5ee19ad29ed606b1ceea8cec9ddaad
Joaquin Gonzalez <jgonzoes@gmail.com>
2019-10-02 07:02:38 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsLiderEntrega
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblTiempoTotal = new System.Windows.Forms.Label();
+            this.lblTiempo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(12, 25);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblTiempoTotal
+            // 
+            this.lblTiempoTotal.AutoSize = true;
+            this.lblTiempoTotal.Location = new System.Drawing.Point(234, 30);
+            this.lblTiempoTotal.Name = "lblTiempoTotal";
+            this.lblTiempoTotal.Size = new System.Drawing.Size(0, 13);
+            this.lblTiempoTotal.TabIndex = 1;
+            // 
+            // lblTiempo
+            // 
+            this.lblTiempo.AutoSize = true;
+            this.lblTiempo.Location = new System.Drawing.Point(150, 30);
+            this.lblTiempo.Name = "lblTiempo";
+            this.lblTiempo.Size = new System.Drawing.Size(69, 13);
+            this.lblTiempo.TabIndex = 2;
+            this.lblTiempo.Text = "Tiempo Total";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(457, 186);
+            this.label1.TabIndex = 3;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 321);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTiempo);
+            this.Controls.Add(this.lblTiempoTotal);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.Text = "Prueba Lider";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblTiempoTotal;
+        private System.Windows.Forms.Label lblTiempo;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿//PRUEBA OLIMPIA
+//La función de la aplicación actual es calcular el saldo final de las cuentas de un "banco", para esto se consume un servicio que devuelve 
+//las transacciones realizas a la cuentas.
+
+//Paso 1: Hacer funcionar la aplicación. Debido al aumento de transacciones y al  colocar al servicio con SSL la aplicación actual esta fallando.
+//Paso 2: Estructurar mejor el codigo. Uso de patrones, buenas practicas, etc.
+//Paso 3: Optimizar el codigo, como se menciono en el paso 1 el aumento de transacciones ha causado que el calculo de los saldos se demore demasiado.
+//Paso 4: Adicionar una barra de progreso al formulario. Actualizar la barra con el progreso del proceso, evitando bloqueos del GUI.
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsLiderEntrega
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {            
+
+            System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
+
+         
+            ServicioPrueba.ServiceClient client = new ServicioPrueba.ServiceClient();
+
+            var resp = client.GetData("usuariop", "passwordp");
+
+            //Variable donse se almacenan los saldos finales
+            List<ServicioPrueba.Saldo> saldos = new List<ServicioPrueba.Saldo>();
+
+            foreach (var transaccion in resp)
+            {
+                var cuentaActual = transaccion.CuentaOrigen;
+                var claveCifrado = client.GetClaveCifradoCuenta("usuariop", "passwordp", cuentaActual);
+                var movimientoActual = Desencripta(claveCifrado, transaccion.TipoTransaccion);
+
+                double saldoActual = -1;
+
+                //Obtenemos el saldo actual de la cuenta
+                foreach (var saldo  in saldos)
+                {
+                    if (cuentaActual == saldo.CuentaOrigen)
+                    {
+                        saldoActual = saldo.SaldoCuenta;
+
+                        double comision = CalcularComision(Convert.ToInt64(transaccion.ValorTransaccion));
+
+                        if (movimientoActual == "Debito")
+                        {
+                            saldo.SaldoCuenta -= transaccion.ValorTransaccion;
+                        }
+                        else
+                        {
+                            saldo.SaldoCuenta += transaccion.ValorTransaccion - comision;
+                        }
+                    }
+                }
+
+                //Si no encuentra lo inserta
+                if(saldoActual == -1)
+                {
+                    ServicioPrueba.Saldo saldo = new ServicioPrueba.Saldo();
+
+
+                    double comision = CalcularComision(Convert.ToInt64(transaccion.ValorTransaccion));
+
+                    saldo.CuentaOrigen = cuentaActual;                   
+                    if (movimientoActual == "Debito")
+                    {
+                        saldo.SaldoCuenta -= transaccion.ValorTransaccion;
+                    }
+                    else
+                    {
+                        saldo.SaldoCuenta += transaccion.ValorTransaccion - comision; 
+                    }
+
+                    saldos.Add(saldo);
+                } 
+            }
+
+
+            sw.Stop();
+            lblTiempoTotal.Text = sw.ElapsedMilliseconds.ToString();
+
+            //Enviamos los saldos finales
+            client.SaveData("usuariop", "passwordp", saldos.ToArray());
+
+
+           
+        }
+
+        public long CalcularComision(long n)
+        {
+            
+            int count = 0;
+            long a = 0;
+            while (count < n)
+            {
+                a = 2;
+
+                long b = 2;
+                int prime = 1;
+                while (b * b <= a)
+                {
+                    if (a % b == 0)
+                    {
+                        prime = 0;
+                        break;
+                    }
+                    b++;
+                }
+                if (prime > 0)
+                {
+                    count++;
+                }
+                a++;
+            }
+            return (--a);
+        }
+
+        public string Desencripta(string ClaveCifrado, string Cadena)
+        {
+            //Este metodo no se requiere estructurar / optimizar
+            byte[] Clave = Encoding.ASCII.GetBytes(ClaveCifrado);
+            byte[] IV = Encoding.ASCII.GetBytes("1234567812345678");
+            
+
+            byte[] inputBytes = Convert.FromBase64String(Cadena);
+            byte[] resultBytes = new byte[inputBytes.Length];
+            string textoLimpio = String.Empty;
+            RijndaelManaged cripto = new RijndaelManaged();
+            using (MemoryStream ms = new MemoryStream(inputBytes))
+            {
+                using (CryptoStream objCryptoStream = new CryptoStream(ms, cripto.CreateDecryptor(Clave, IV), CryptoStreamMode.Read))
+                {
+                    using (StreamReader sr = new StreamReader(objCryptoStream, true))
+                    {
+                        textoLimpio = sr.ReadToEnd();
+                    }
+                }
+            }
+            return textoLimpio;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
